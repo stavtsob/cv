2bba8f1c7278b986aaea09de87194d7e8839df68
Stavros Tsompanidis <stavtsob@gmail.com>
2026-01-27 14:10:05 +0200

    fix: set basePath to "/cv" in Next.js configuration

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   output: "export",
-  basePath: process.env.NODE_ENV === "production" ? "" : "",
+  basePath: "/cv",
 };
 
 export default nextConfig;
